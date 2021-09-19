42d1006a90769d05cd9561a75f009ce38d7f5a5d
joeldevel <christianjoel.developer@gmail.com>
2021-09-18 23:02:10 -0300

    add bootstrap 5 cdn to index.html

--- src/Routes.js
+++ src/Routes.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 import App from "./App";
 import Profile from "./Profile";
 import Forum from './Forum';
@@ -6,11 +6,26 @@ import Forum from './Forum';
 const Routes = () => {
   return (
     <BrowserRouter>
+    	<>
+    	<nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/profile">Profile</Link>
+            </li>
+            <li>
+              <Link to="/forum">Forum</Link>
+            </li>
+          </ul>
+        </nav>
       <Switch>
         <Route exact path="/" component={App} />
         <Route exact path="/profile" component={Profile} />
         <Route exact path="/forum" component={Forum}/>
       </Switch>
+      </>
     </BrowserRouter>
   );
 };
