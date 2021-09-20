ad5a947bc25501215df8c471897df0e1d67af76d
joeldevel <christianjoel.developer@gmail.com>
2021-09-20 14:19:00 -0300

    add state to individual post and show post content

--- src/Post.js
+++ src/Post.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 const Profile = (props) => {
+	const [postContent, setPostContent] = useState({});
+
+	useEffect(()=> {
+		const postId = props.match.params.id;
+		fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`)
+		.then(response => response.json())
+		.then(postData => setPostContent(postData));
+	}, []);
+
+	//console.log(props.match.params.id);
 	return (
-		<div>this is a post</div>	
+		<div className="card">
+			<h1>this is post {postContent.id}</h1>
+			<p>{postContent.title}</p>
+			<p>{postContent.text}</p>
+		</div>	
 	);
 }
 
