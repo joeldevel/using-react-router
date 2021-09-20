8dc41d30d09e691debfd7da8f7872ee32b4c7ece
joeldevel <christianjoel.developer@gmail.com>
2021-09-20 14:25:31 -0300

    add navigation from post excerpt to post and post to forum

--- src/Post.js
+++ src/Post.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from 'react';
+import {Link} from 'react-router-dom';
 
 const Profile = (props) => {
 	const [postContent, setPostContent] = useState({});
@@ -10,13 +11,15 @@ const Profile = (props) => {
 		.then(postData => setPostContent(postData));
 	}, []);
 
-	//console.log(props.match.params.id);
 	return (
+		<>
+		<Link to="/forum">Go back to forum</Link>
 		<div className="card">
 			<h1>this is post {postContent.id}</h1>
 			<p>{postContent.title}</p>
-			<p>{postContent.text}</p>
-		</div>	
+			<p>{postContent.body}</p>
+		</div>
+		</>	
 	);
 }
 
--- src/PostExcerpt.js
+++ src/PostExcerpt.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 const PostExcerpt = (props) => {
 	return (
+		<Link to={`/forum/${props.postData.id}`}>
 		<div className="card">
 			<p>Written by {props.postData.userId}</p>
 			<h2>{props.postData.title}</h2>
 		</div>
+		</Link>
 	);
 }
 
